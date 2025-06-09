58ee2ac6cd9805172798f6a84747ce687eab98f5
Antonia Thyssen <wi24b090@technikum-wien.at>
2025-06-09 08:41:36 +0200

    US-101 added Class TicTacToe with -Player player 1 -Player player 2 -Player current player -Board board and methods -constructor TicTacToe() -start() -switchCurrentPlayer()

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Player player1=new Player('X');
+    private Player player2=new Player('O');
+    private Player currentPlayer=player1;
+    private Board board=new Board();
+
+    public TicTacToe() {
+    }
+
+    public void start() {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println( "Welcome to TicTacToe!");
+        System.out.println("Player 1: "+player1.getMarker());
+        System.out.println("Player 2: "+player2.getMarker());
+
+        for (int i = 0; i < 9; i++) {
+            System.out.printf("Current player: %s %s%n",currentPlayer,currentPlayer.getMarker()+"");
+
+            boolean isEmpty=false;
+
+            while(!isEmpty) {
+                System.out.println("Enter the coordinates: ");
+                int row = scn.nextInt();
+                int col = scn.nextInt();
+                if (board.isCellEmpty(row, col)) {
+                    board.place(row, col, currentPlayer.getMarker());
+                    switchCurrentPlayer();
+                } else {
+                    System.out.println("This cell is occupied! Try again...");
+                }
+                isEmpty = board.isFull(row, col);
+            }
+        }
+
+        System.out.println("Thank you for playing!");
+
+
+        scn.close();
+    }
+
+    public void switchCurrentPlayer() {
+        if(currentPlayer==player1) {
+            currentPlayer=player2;
+        } else if (currentPlayer==player2) {
+            currentPlayer=player1;
+        }
+    }
+}
