f3ff5d74bc942cd919c8f341f1083374873e3101
Antonia Thyssen <wi24b090@technikum-wien.at>
2025-06-09 08:14:54 +0200

    US-101 added Constructor Board() to class Board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,6 +1,14 @@
 public class Board {
     private char[][] cells=new char[3][3];
 
+    public Board() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j]=' ';
+            }
+        }
+    }
+
     public boolean isCellEmpty(int row, int col) {
         return cells[row][col]==' ';
     }
