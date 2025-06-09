a4413ebd1b4c82f4555a1fe40039907c23d90791
ajm2204 <wi24b133@technikum-wien.at>
2025-06-09 18:38:09 +0200

    US-103 add hasWinner to class TicTacToe checks game state according to US-103

--- src/main/java/src/TicTacToe.java
+++ src/main/java/src/TicTacToe.java
@@ -52,4 +52,33 @@ public class TicTacToe {
             currentPlayer=player1;
         }
     }
+
+    public boolean hasWinner() {
+        char[][] cells = board.getCells();
+
+        // Zeilen prüfen
+        for (int i = 0; i < 3; i++) {
+            if (cells[i][0] != ' ' &&
+                    cells[i][0] == cells[i][1] &&
+                    cells[i][1] == cells[i][2]) {
+                return true;
+            }
+        }
+
+        // Spalten prüfen
+        for (int i = 0; i < 3; i++) {
+            if (cells[0][i] != ' ' &&
+                    cells[0][i] == cells[1][i] &&
+                    cells[1][i] == cells[2][i]) {
+                return true;
+            }
+        }
+
+        // Diagonale prüfen
+        if (cells[0][0] != ' ' && cells[0][0] == cells[1][1] && cells[1][1] == cells[2][2]) return true;
+        if (cells[0][2] != ' ' && cells[0][2] == cells[1][1] && cells[1][1] == cells[2][0]) return true;
+
+        return false;
+    }
+
 }
