3dd53d12f73ef4b368ba03528c4088bf8cbca465
fesit <wi24b079@technikum-wien.at>
2025-06-09 17:58:41 +0200

    US-102,  add printBoard to Board

--- src/main/java/src/Board.java
+++ src/main/java/src/Board.java
@@ -1,26 +1,41 @@
 package src;
 
 public class Board {
-    private char[][] cells=new char[3][3];
+    private char[][] cells = new char[3][3];
 
     public Board() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                cells[i][j]=' ';
+                cells[i][j] = ' ';
             }
         }
     }
 
     public boolean isCellEmpty(int row, int col) {
-        return cells[row][col]==' ';
+        return cells[row][col] == ' ';
     }
 
     public boolean isFull(int row, int col) {
-        return cells[row][col]!=' ';
+        return cells[row][col] != ' ';
     }
 
     public void place(int row, int col, char sign) {
-        cells[row][col]=sign;
+        cells[row][col] = sign;
     }
 
+
+    public void printBoard() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append("|");
+            for (int j = 0; j < 3; j++) {
+                sb.append(cells[i][j]).append(" |");
+            }
+            if (i < 2) {
+                sb.append(System.lineSeparator());
+            }
+        }
+        System.out.println(sb);
+    }
 }
+
